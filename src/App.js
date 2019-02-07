d36ab54eaa14a623a5bfb7747f668da9342dccaa
julien <j.banchetti@hotmail.fr>
2019-02-07 13:11:05 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Spring } from 'react-spring';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Spring from={{ opacity:0, marginTop: -1000 }} to={{ opacity:1, marginTop: 0 }}>
+        {
+          props => (
+            <div className='App' style={ props }>
+              <div>
+                <header className='App-header'>
+                  <img src={logo} className='App-logo' alt='logo' />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                   className="App-link"
+                   href='https://reactjs.org'
+                   target='_blank'
+                   rel='noopener noreffer'
+                  >
+                    Leana React
+                  </a>
+                </header>
+              </div>
+            </div>
+          )
+        }
+      </Spring>
     );
   }
 }
